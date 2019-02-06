880a4fe7a05e99c892d888c7b34e1cb0794993f0
Joost Bremmer <contact@madeofmagicandwires.online>
2019-02-06 12:07:58 +0100

    Add option to reset visibility and various optimisations
    Signed-off-by: Joost Bremmer <contact@madeofmagicandwires.online>

--- app/src/main/java/online/madeofmagicandwires/potatohead/MainActivity.java
+++ app/src/main/java/online/madeofmagicandwires/potatohead/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.util.SparseIntArray;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.GridLayout;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -14,7 +15,7 @@ import java.io.ObjectOutputStream;
 
 import java.io.Serializable;
 
-
+@SuppressWarnings({"WeakerAccess", "JavaDoc"})
 public class MainActivity extends AppCompatActivity {
 
     /**
@@ -23,20 +24,29 @@ public class MainActivity extends AppCompatActivity {
      * @see java.io.Serializable
      * @see "https://stackoverflow.com/a/21574953" for inspiration.
      */
-    public class SerializableSparseIntArray extends SparseIntArray implements Serializable{
+    @SuppressWarnings({"unused"})
+    public static class SerializableSparseIntArray extends SparseIntArray implements Serializable{
 
+        /** needed for custom serializations **/
         private static final long serialVersionUID = 5939429315080188394L;
 
+        /**
+         * Constructor specifying an initial size to allocate
+         * @param initialCapacity the initial amount of capacity to pre-allocate memory for
+         */
         public SerializableSparseIntArray(int initialCapacity){
             super(initialCapacity);
         }
 
+        /**
+         * Standard no-frills constructor
+         */
         public SerializableSparseIntArray() {
             super();
         }
 
         /**
-         * Serializable required methods
+         * Writes object data to OutputStream
          */
         private void writeObject(ObjectOutputStream oos) throws IOException {
             Object[] data = new Object[size()];
@@ -48,17 +58,25 @@ public class MainActivity extends AppCompatActivity {
             oos.writeObject(data);
         }
 
+        /**
+         * Reads object data from input stream
+         * @param ois the inputstream to read from
+         * @throws IOException
+         * @throws ClassNotFoundException
+         */
         private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
             Object[] data = (Object[]) ois.readObject();
-            for(int i=0;i<data.length;i++) {
-                Object[] pair = (Object[]) data[i];
+            for(Object aData : data) {
+                Object[] pair = (Object[]) aData;
                 this.append((int) pair[0], (int) pair[1]);
             }
 
         }
 
     }
-    public SerializableSparseIntArray visibilityState;
+    /** Used to save the toggled body parts' id and its visibility state **/
+    private SerializableSparseIntArray visibilityState;
+
 
     /**
      * Restore visibility state of previously toggled ImageViews.
@@ -72,12 +90,14 @@ public class MainActivity extends AppCompatActivity {
         // Restore visibility state.
         // On initial run this doesn't exist, so create an empty SparseIntArray instead.
 
-        try {
+        if(savedInstanceState != null && savedInstanceState.containsKey("visibility")) {
             visibilityState = (SerializableSparseIntArray)
                     savedInstanceState.getSerializable("visibility");
-            Log.d("potato onCreate", visibilityState.toString());
-            restoreVisibilityState(visibilityState);
-        } catch(NullPointerException e) {
+            if(visibilityState != null) {
+                Log.d("retrieved visibility states:", visibilityState.toString());
+                restoreVisibilityState(visibilityState);
+            }
+        } else  {
             Log.d("potato OnCreate", "savedInstance was null, creating new visibilityState");
             visibilityState = new SerializableSparseIntArray();
         }
@@ -90,7 +110,9 @@ public class MainActivity extends AppCompatActivity {
      * @param state SparseIntArray to store reference in
      */
     public void storeVisibilityState(View v, SparseIntArray state){
-        state.put(v.getId(), v.getVisibility());
+        if(v.getId() != View.NO_ID) {
+            state.put(v.getId(), v.getVisibility());
+        }
     }
 
     /**
@@ -104,15 +126,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Searches Resources for a View with the id named String
-     * example: findViewByIdString("foobar") returns R.id.foobar
-     * @param  idString: a String of the id
+     * Searches Resources for a View id by name
+     * e.g: findViewByIdString("foobar") returns R.id.foobar
+     * @param  idString: a id name of the View to return
      * @return the found View
      * @see 'https://stackoverflow.com/a/4865350' for inspiration
      * @throws Resources.NotFoundException;
      *
      */
-    public View findViewByIdString(String idString){
+    public View findViewByIdString(String idString) throws Resources.NotFoundException {
 
         int resId = getResources().getIdentifier(idString, "id", getPackageName());
         if(resId != 0){
@@ -136,15 +158,34 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     /**
-     * Onclick function for checkboxes. Finds the corresponding ImageView and toggles its visibility.
+     * OnClick method for the Reset button
+     * Resets all toggled views back to invisible
+     * @param v view to be clicked, necessary for the onclick interface, but not used.
+     */
+    public void resetVisibility(View v){
+        for(int i=0;i<visibilityState.size();i++) {
+            findViewById(visibilityState.keyAt(i)).setVisibility(View.INVISIBLE);
+            visibilityState.put(visibilityState.keyAt(i), View.INVISIBLE);
+        }
+        GridLayout grid = findViewById(R.id.GRID);
+        for(int i=0;i<grid.getChildCount();i++){
+            ((CheckBox)grid.getChildAt(i)).setChecked(false);
+        }
+    }
+
+
+
+    /**
+     * Onclick method for checkboxes. Finds the corresponding ImageView and toggles its visibility.
      * Used to be checkClicked(); renamed to better descriptor
      *
-     * @param v Checkbox object that called the function.
+     * @param v Checkbox object that called the method.
      */
     public void toggleBodyPart(View v ) {
         CheckBox box = (CheckBox) v;
-        String boxText = box.getText().toString();
+        String boxText = box.getTag().toString().toLowerCase();
 
         try {
             // find corresponding imageView then toggle its visibility.
@@ -158,8 +199,6 @@ public class MainActivity extends AppCompatActivity {
         } catch(Resources.NotFoundException e){
             Log.w("potato", e.getMessage());
         }
-
-        // Log.d("potato", boxText);
 
 
     }
